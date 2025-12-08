b763114e3cff8085bf56421ff6b481a501bab5e2
xuerzong <xuerzong@gmail.com>
2025-12-08 17:09:40 +0800

    feat: Landing page

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,8 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://redis-studio.xuco.me',
+  base: '/',
+  i18n: {
+    locales: ['zh', 'en'],
+    defaultLocale: 'en',
+  },
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
+  },
+  routing: {
+    prefixDefaultLocale: false,
   },
 })
